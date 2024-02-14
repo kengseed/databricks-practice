add47edcc2b593888e428670dce348b23e9a0607
kengseed <kengseed>
2024-02-14 09:12:26 +0000

    Test hello notebook commit

---
+++ basic-practice/Hello Keng Notebook.py
@@ -0,0 +1,18 @@
+# Databricks notebook source
+print("echo")
+
+
+# COMMAND ----------
+
+import random
+import matplotlib.pyplot as plt
+
+# Generate a list of random numbers
+random_numbers = [random.randint(1, 100) for _ in range(100)]
+
+# Plot a histogram of the random numbers
+plt.hist(random_numbers, bins=10)
+plt.xlabel('Number')
+plt.ylabel('Frequency')
+plt.title('Histogram of Random Numbers')
+plt.show()
